c0d80ae90717e6473de5686ced185d4f2fb62095
sidemt <25644062+sidemt@users.noreply.github.com>
2020-06-29 16:41:40 +0900

    Add security features

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var cors        = require('cors');
+var helmet      = require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -11,7 +12,8 @@ var runner            = require('./test-runner');
 var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0'}));
+app.use(halmet.noCache());
 app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
@@ -26,9 +28,9 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
