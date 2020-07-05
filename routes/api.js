2fb98cda72ff3b10bea0643cea2062a47fd91b43
sidemt <25644062+sidemt@users.noreply.github.com>
2020-07-05 10:12:10 +0900

    Add other routes

--- routes/api.js
+++ routes/api.js
@@ -21,27 +21,49 @@ module.exports = function (app) {
     .get(function (req, res){
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, client){
+        let db = client.db('personal-library');
+        let collection = db.collection('books');
+        collection.find({}).toArray(function(err, docs){
+          res.json(docs);
+          db.close();
+        });
+      });
     })
 
     .post(function (req, res){
       var title = req.body.title;
       //response will contain new book object including atleast _id and title
-      var book = {
-        title: title
+      let book = {
+        title: title,
+        comments: [],
+        commentcount: 0
       };
       MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, client){
         let db = client.db('personal-library');
-        var collection = db.collection('books');
+        let collection = db.collection('books');
         collection.insertOne(book, function(err, doc){
           book._id = doc.insertedId;
           res.json(book);
+          db.close();
         })
       })
     })
 
     .delete(function(req, res){
       //if successful response will be 'complete delete successful'
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, client){
+        let db = client.db('personal-library');
+        let collection = db.collection('books');
+        collection.findOneAndDelete({}, function(err, doc){
+          if (err) {
+            console.error(err);
+          } else {
+            res.send("complete delete successful");
+          }
+          db.close();
+        });
+      });
     });
 
 
@@ -49,18 +71,68 @@ module.exports = function (app) {
   app.route('/api/books/:id')
     .get(function (req, res){
       var bookid = req.params.id;
+      console.log("bookid", bookid)
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, client){
+        let db = client.db('personal-library');
+        let collection = db.collection('books');
+        collection.findOne({ "_id": ObjectId(bookid) }, function(err, doc){
+          if (err) {
+            console.error(err);
+          } else if (doc === null) {
+            res.send("no book exists")
+          } else {
+            res.json(doc);
+          }
+          db.close();
+        });
+      });
     })
 
     .post(function(req, res){
       var bookid = req.params.id;
       var comment = req.body.comment;
       //json res format same as .get
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, client){
+        let db = client.db('personal-library');
+        let collection = db.collection('books');
+        collection.findOneAndUpdate({ "_id": ObjectId(bookid) },
+          { $push: {comments: comment },
+                    $inc: {commentcount: 1 }
+          }, function(err, doc){
+          if (err) {
+            console.error(err);
+          } else {
+            let comments = doc.value.comments;
+            comments.push(comment);
+            let json = {
+              "_id": doc.value._id,
+              "title": doc.value.title,
+              "commentcount": doc.value.commentcount + 1,
+              "comments": comments
+            }
+            res.json(json);
+          }
+          db.close();
+        })
+      })
     })
 
     .delete(function(req, res){
       var bookid = req.params.id;
       //if successful response will be 'delete successful'
+      MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, client){
+        let db = client.db('personal-library');
+        let collection = db.collection('books');
+        collection.findOneAndDelete({ "_id": ObjectId(bookid) }, function(err, doc){
+          if (err) {
+            console.error(err);
+          } else {
+            res.send("delete successful");
+          }
+          db.close();
+        });
+      });
     });
 
 };
